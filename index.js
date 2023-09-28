0d70d7cefa47537802f7cdeaa897cdb0b44e760f
muska <n01540458@humbermail.ca>
2023-09-27 21:16:14 -0400

    created function to add a product

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ let SERVER_NAME = 'productsApi'
 let PORT = 3000;
 let HOST = '127.0.0.1';
 
+// Define get and post counters
+let getRequests = 0;
+let postRequests = 0;
+let deleteRequests = 0;
+
 // Include errors and restify
-let restifyErrors = require('restify-errors');
+let errors = require('restify-errors');
 let restify = require('restify')
 
 // Get an engine for products
-, products = require('save')('products')
+, productsSave = require('save')('products')
 
 // Create restify server
 , server = restify.createServer({name: SERVER_NAME})
@@ -25,4 +30,55 @@ server.listen(PORT, HOST, function() {
 // Using the server
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
+
+// FUNCTIONS TO HANDLE OPERATIONS ON PRODUCTS
+
+// Create a new product
+server.post('/products', function (req, res, next) {
+    postRequests = postRequests + 1;
+
+    console.log('');
+    console.log('');
+    console.log('CREATING A NEW PRODUCT !!');
+    console.log('Endpoint - http://127.0.0.1:3000/products');
+    console.log('POST : params - ' + JSON.stringify(req.params));
+    console.log('POST : Sending product payload - ' + JSON.stringify(req.body));
+    console.log('Request counter --> GET: ' + getRequests + ', POST: ' + postRequests + ', DELETE: ' + deleteRequests);
+
+    // validation of manadatory fields
+    if (req.body.productId === undefined ) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new errors.BadRequestError('product id must be supplied'))
+    }
+    if (req.body.name === undefined ) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new errors.BadRequestError('product name must be supplied'))
+    }
+    if (req.body.price === undefined ) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new errors.BadRequestError('product price must be supplied'))
+    }
+    if (req.body.quantity === undefined ) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new errors.BadRequestError('product quantity must be supplied'))
+    }
+
+    let newProduct = {
+            productId: req.body.productId, 
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity
+        }
+
+    // Create the product using the persistence engine
+    productsSave.create( newProduct, function (error, product) {
+
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new Error(JSON.stringify(error.errors)))
+
+        // Send the product if no issues
+        res.send(201, product)
+    })
+})
+
 
